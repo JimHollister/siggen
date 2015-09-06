5f606004511aa2a9547eb88adc23adc51e47cdec
Jim Hollister <jim.hollister@earthlink.net>
2015-09-06 17:25:26 -0500

    Implemented frequency to freq word conversion.

--- siggen/siggen.c
+++ siggen/siggen.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <avr/io.h>
 #include "common.h"
 #include <util/delay.h>
@@ -6,7 +7,7 @@
 #include "dds.h"
 //#include <stdfix.h>
 
-extern void mul_32x32();
+extern unsigned long long mul_32x32(uint32_t multiplicand, uint32_t multiplier);
 
 /*
 * Initialization of the USI peripheral. USI is used to communicate with the DDS chip and the LCD.
@@ -15,6 +16,19 @@ void usi_initialize()
 {
 	DDRB |= _BV(DDB6) | _BV(DDB7);							// DO and USCK as outputs
 	USICR = _BV(USIWM0);									// USI in 3-wire mode, software clock strobe
+}
+
+unsigned long word_freq_ratio = 0xE5109EC2;		// Word/freq ratio of 3.57913941333333 in Q2.30 fixed point
+
+unsigned long calc_freq_word(unsigned long freq) {
+	// Supplied freq value is in unsigned 32-bit format, also known as Q32.0 fixed point
+	unsigned long long temp = mul_32x32(freq, word_freq_ratio);		// Multiply freq by ratio to get freq word value in Q34.30 fixed point
+	temp = temp >> 29;												// Right shift 29 times to get freq word in Q63.1 fixed point
+	bool roundup = false;											// Is the rightmost (1/2 place) bit set? If so, round up the final result
+	if (temp & 0x000001) {roundup = 1;}
+	unsigned long freq_word = (unsigned long)(temp >> 1);	// Right shift 1 time to get freq word in Q64.0 fixed point. Cast to Q32.0.
+	if (roundup == true) {freq_word++;}						// If called for, round the freq word up by one
+	return freq_word;
 }
 
 int main(void)
@@ -31,16 +45,29 @@ int main(void)
 //	pin_test4();
 //	pin_test5();
 
-// Store ratio in a Q32.32 fixed point variable
-//volatile unsigned long accum ratio = 3.57913941333333lk;
-//volatile unsigned accum ratio = 3.57913941333333lk;
-// Desired frequency is also in a Q32.32 fixed point variable
-//volatile unsigned long accum freq = 12345678lk;
-//volatile unsigned accum freq = 1234lk;
-// Calculate frequency word value
-//volatile unsigned long accum word = ratio * freq;
+	volatile unsigned long freq_word = 0;
+	freq_word = calc_freq_word(30000000);	// freq_word should be 107374182
+	freq_word = calc_freq_word(1000000);	// freq_word should be 3579139
+	freq_word = calc_freq_word(250000);		// freq_word should be 894785
+	freq_word = calc_freq_word(1000);		// freq_word should be 3579
+	freq_word = calc_freq_word(25);			// freq_word should be 89
+	freq_word = calc_freq_word(4);			// freq_word should be 14
+	freq_word = calc_freq_word(1);			// freq_word should be 4
+	freq_word = calc_freq_word(0);			// freq_word should be 0
 
-	mul_32x32();
+	volatile unsigned long long result;
+
+	// Answer should be 0
+	result = mul_32x32(0x12345678, 0);
+
+	// Answer should be 305419896
+	result = mul_32x32(0x12345678, 1);
+
+	// Answer should be 693779765864729976	
+	result = mul_32x32(0x12345678, 0x87654321);
+
+	// Answer should be 9223372036854775807
+	result = mul_32x32(0xFFFFFFFF, 0xFFFFFFFF);
 
 	while(1)
 	{
